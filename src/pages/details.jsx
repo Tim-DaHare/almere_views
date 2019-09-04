0e8f6142de134ff1acc33f62a7119abcab9574a6
Tim de Haas <tim.de.haas@hotmail.nl>
2019-09-04 11:52:36 +0200

    added page boilerplate for details page

---
+++ src/pages/details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image } from 'react-native'
+
+const Details = () => {
+    return (
+        <React.Fragment>
+              
+        </React.Fragment>
+    )
+}
+
+export default Details

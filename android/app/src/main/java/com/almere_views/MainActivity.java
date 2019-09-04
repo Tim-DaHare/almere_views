36dc76c9c334710e1670cab750ff8226cdf17e58
Tim de Haas <tim.de.haas@hotmail.nl>
2019-09-04 10:42:27 +0200

    init project

---
+++ android/app/src/main/java/com/almere_views/MainActivity.java
@@ -0,0 +1,15 @@
+package com.almere_views;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "almere_views";
+    }
+}
